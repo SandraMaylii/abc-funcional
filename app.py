a786534c7fa1ee18c94b1e416349217018256010
SandraMaylii <srodriguezg8@upao.edu.pe>
2025-07-02 16:34:09 -0500

    Corrigiendo para despliegue 7

--- app.py
+++ app.py
@@ -221,7 +221,10 @@ def estado():
 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Puerto que asigna Cloud Run
-    app.run(host='0.0.0.0', port=port, debug=False)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # default para Render
+    app.run(host="0.0.0.0", port=port)
+
 
